a30b90848f2c276172227b5a8a851ba6f1c34968
Audrow Nash <audrow@hey.com>
2022-04-16 08:52:00 -0700

    Add copyright
    Signed-off-by: Audrow Nash <audrow@hey.com>

---
+++ components/Copyright/Copyright.stories.tsx
@@ -0,0 +1,34 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react'
+import React from 'react'
+import Copyright from './Copyright'
+
+export default {
+  title: 'App/Copyright',
+  component: Copyright,
+} as ComponentMeta<typeof Copyright>
+
+const Template: ComponentStory<typeof Copyright> = (args) => <Copyright {...args} />
+
+export const OnlyName = Template.bind({})
+OnlyName.args = {
+  holderName: 'Your Company',
+}
+
+export const NameAndUrl = Template.bind({})
+NameAndUrl.args = {
+  holderName: 'Your Company',
+  holderUrl: 'https://your-company.com',
+}
+
+export const NameAndYear = Template.bind({})
+NameAndYear.args = {
+  holderName: 'Your Company',
+  year: 2020,
+}
+
+export const NameAndUrlAndYear = Template.bind({})
+NameAndUrlAndYear.args = {
+  holderName: 'Your Company',
+  holderUrl: 'https://your-company.com',
+  year: 2020,
+}
---
+++ components/Copyright/Copyright.tsx
@@ -0,0 +1,32 @@
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+
+export type CopyrightProps = {
+  holderName: string
+  holderUrl?: string
+  year?: number
+}
+
+export default function Copyright(props: CopyrightProps) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {'Copyright © '}
+      {props.holderUrl ? (
+        <Link color="inherit" href={props.holderUrl}>
+          {props.holderName}
+        </Link>
+      ) : (
+        props.holderName
+      )}
+      {' '}
+      {props.year ? props.year : new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
